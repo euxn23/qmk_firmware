7bf0be722978889f27bf6b9ad4d0c9478d9c4f10
euxn23 <euxn23@gmail.com>
2021-07-21 17:14:31 +0900

    fix(moonlander): fix shift behavior

--- keyboards/moonlander/keymaps/euxn/keymap.c
+++ keyboards/moonlander/keymaps/euxn/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,              KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,
 LALT_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(SYMB),         MO(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,            KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-LSFT_T(KC_MHEN), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_HENK),
-        KC_LALT, _______, _______, _______, KC_RPRN,          MO(SYMB),         MO(SYMB),          KC_LPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
+        KC_MHEN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_HENK,
+        KC_LALT, _______, _______, KC_LSFT, KC_RPRN,          MO(SYMB),         MO(SYMB),          KC_LPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
                                      LSFT_T(KC_SPC), KC_LGUI, KC_RBRC,           KC_LBRC, KC_SPC,  LSFT_T(KC_SPC)
     ),
 
