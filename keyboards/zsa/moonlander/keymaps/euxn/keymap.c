b5c8c3ceb60119c4fc6f1094a53996136e660540
euxn23 <euxn23@gmail.com>
2025-01-05 05:36:41 +0900

    feat: improve meta layer

--- keyboards/zsa/moonlander/keymaps/euxn/keymap.c
+++ keyboards/zsa/moonlander/keymaps/euxn/keymap.c
@@ -5,26 +5,26 @@
 
 enum layers {
     BASE,  // default layer
-    SYMB,  // symbols
+    META,  // meta
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,              KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,
- LALT_T(KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(SYMB),         MO(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+ LALT_T(KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(META),         MO(META), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,           KC_LBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_MHEN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_HENK,
+LT(META,KC_MHEN),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LT(META,KC_HENK),
         KC_LALT, _______, _______, KC_F7,   KC_RPRN,          KC_GRV,            KC_QUOT,          KC_LPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
-                                     LSFT_T(KC_SPC), KC_LGUI, MO(SYMB),         MO(SYMB), KC_SPC,  LSFT_T(KC_SPC)
+                                     LSFT_T(KC_SPC), KC_LGUI, MO(META),         MO(META), KC_SPC,  LSFT_T(KC_SPC)
     ),
 
-    [SYMB] = LAYOUT_moonlander(
+    [META] = LAYOUT_moonlander(
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RGB_TOG,
-        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______,           _______, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,          _______,           _______,          KC_MPLY, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
+        _______, _______, _______, _______, _______,          _______,           _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
                                             _______, _______, _______,           _______, _______, _______
     ),
 };
